9bb1d627c9fec8a19322e11d335407a02c061e57
UmuT5513 <umutagriman0@gmail.com>
2026-03-12 10:46:01 +0300

    feat: Controller bağlantısı sağla, Index te göster

---
+++ hafta3/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using hafta3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hafta3.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private ProductRepository _repository;
+
+        public ProductController()
+        {
+            _repository = new ProductRepository();
+            if(!_repository.GetAllProducts().Any()) // Eğer repo boş ise;
+            {
+                _repository.Add(new Product { Id = 1, Name = "Laptop", Price = 10000, Stok = 10 });
+                _repository.Add(new Product { Id = 2, Name = "Telefon", Price = 5000, Stok = 20 });
+                _repository.Add(new Product { Id = 3, Name = "Tablet", Price = 3000, Stok = 15 });
+                }
+        }););
+        public IActionResult Index()
+        {
+            var urunler = _repository.GetAllProducts();
+            return View(urunler);
+        }
+    }
+}
