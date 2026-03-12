e99809be0b85b0e09d0db58a6af0e0708ad3b29a
UmuT5513 <umutagriman0@gmail.com>
2026-03-12 10:31:29 +0300

    feat: update operation

--- hafta3/Models/ProductRepository.cs
+++ hafta3/Models/ProductRepository.cs
@@ -19,10 +19,31 @@
             var hasProduct = _products.FirstOrDefault(temp => temp.Id == id);
 
             if (hasProduct == null)
-                @* ürün bulunamadı *@
+            {
+                // ürün bulunamadı
                 throw new Exception($"{id}'li ürün bulunamadı");
+            }
 
             _products.Remove(hasProduct);
+        }
+
+        public void Update(Product güncellenecekUrun)
+        {
+            var hasProduct = _products.FirstOrDefault(temp => temp.Id == güncellenecekUrun.Id);
+
+            if (hasProduct == null)
+            {
+                // ürün bulunamadı
+                throw new Exception($"{güncellenecekUrun.Id}'li ürün bulunamadı");
+            }
+
+            /*hasProduct.Name = güncellenecekUrun.Name;
+            hasProduct.Price = güncellenecekUrun.Price;
+            hasProduct.Stok = güncellenecekUrun.Stok;*/
+
+            var index = _products.IndexOf(hasProduct);
+            _products[index] = güncellenecekUrun;
+            // _products.Remove(hasProduct);
         }
     }
 }
