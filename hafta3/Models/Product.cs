d1686cfc209d71f9015825547c61ddc30b37e3a8
UmuT5513 <umutagriman0@gmail.com>
2026-03-12 10:20:01 +0300

    feat: temel ekle ve kaldır işlemleri

--- hafta3/Models/Product.cs
+++ hafta3/Models/Product.cs
@@ -2,6 +2,11 @@
 {
     public class Product
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Stok { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
---
+++ hafta3/Models/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace hafta3.Models
+{
+    public class ProductRepository
+    {
+        private static List<Product> _products = new List<Product>();
+
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public void Add(Product yeniUrun)
+        {
+            _products.Add(yeniUrun);
+        }
+
+        public void Remove(int id)
+        {
+            var hasProduct = _products.FirstOrDefault(temp => temp.Id == id);
+
+            if (hasProduct == null)
+                @* ürün bulunamadı *@
+                throw new Exception($"{id}'li ürün bulunamadı");
+
+            _products.Remove(hasProduct);
+        }
+    }
+}
