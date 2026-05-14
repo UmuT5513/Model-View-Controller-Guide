4779e6a3a39efc9491c42eaae0ccea03931e8eff
Umut Ağrıman <127418691+UmuT5513@users.noreply.github.com>
2026-05-14 12:11:06 +0300

    feat: Add tag helpers

---
+++ hafta3/TagHelpers/ItalicTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace hafta3.TagHelpers
+{
+    public class ItalicTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.SetHtmlContent("<i>");
+            output.PostContent.SetHtmlContent("</i>");
+        }
+    }
+}
---
+++ hafta3/TagHelpers/ProductShowTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace hafta3.TagHelpers
+{
+    public class ProductShowTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Content.SetHtmlContent("<ul>" +
+                "<li>Kalem</li>" +
+                "<li>3 TL</li>" +
+                "<li>Stok: 1000 </li>" +
+                "</ul>");
+        }
+    }
+}
